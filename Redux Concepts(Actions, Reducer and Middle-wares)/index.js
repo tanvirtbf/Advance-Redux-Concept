46ec4f49df66ebaa0a3ebc38ad5d1708c41274ec
tanvirtbf <tanver018765@gmail.com>
2024-04-11 06:17:11 +0600

    start new day

--- Redux Concepts(Actions, Reducer and Middle-wares)/index.js
+++ Redux Concepts(Actions, Reducer and Middle-wares)/index.js
@@ -1,1 +1,24 @@
-console.log('hello')
+import {createStore} from 'redux'
+
+const store = createStore(reducer)
+
+const history = []
+
+//reducer
+function reducer(state = {amount: 0}, action){
+  if(action.type === 'increment'){
+    state.amount += action.payload
+  }
+  return state
+}
+
+
+//global state
+
+store.subscribe(()=>{
+  console.log(store.getState())
+})
+
+
+store.dispatch({type: 'increment', payload: 2})
+
