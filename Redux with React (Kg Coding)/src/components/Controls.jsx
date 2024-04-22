7d67fb761d332273d4d604e0cf877490e17eed2e
tanvirtbf <tanver018765@gmail.com>
2024-04-22 11:13:20 +0600

    bug fixed...

--- Redux with React (Kg Coding)/src/components/Controls.jsx
+++ Redux with React (Kg Coding)/src/components/Controls.jsx
@@ -4,6 +4,15 @@ import { useDispatch } from "react-redux";
 const Controls = () => {
   const dispatch = useDispatch();
   const inputRef = useRef();
+  function handleADD(){
+    const number = inputRef.current.value
+    dispatch({type: 'ADD', payload: Number(number)})
+    inputRef.current.value = ""
+  }
+  function handleSUBTRACT(){
+    dispatch({type: 'SUBTRACT', payload: Number(inputRef.current.value)})
+    inputRef.current.value = ""
+  }
   return (
     <>
       <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
@@ -29,10 +38,10 @@ const Controls = () => {
           className="number-input"
           ref={inputRef}
         />
-        <button type="button" className="btn btn-info" onClick={() => dispatch({type: 'ADD', payload: inputRef.current.value})}>
+        <button type="button" className="btn btn-info" onClick={handleADD}>
           ADD
         </button>
-        <button type="button" className="btn btn-danger" onClick={() => dispatch({type: 'SUBTRACT', payload: inputRef.current.value})}>
+        <button type="button" className="btn btn-danger" onClick={handleSUBTRACT}>
           Subtract
         </button>
       </div>
