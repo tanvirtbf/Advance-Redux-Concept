b06160e224f521e298741c1da52b7178a9b162fd
tanvirtbf <tanver018765@gmail.com>
2024-04-22 10:09:39 +0600

    create controls.jsx

--- Redux with React (Kg Coding)/src/App.jsx
+++ Redux with React (Kg Coding)/src/App.jsx
@@ -1,35 +1,21 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Container from "./components/Container";
+import Controls from "./components/Controls";
 import DisplayCounter from "./components/DisplayCounter";
 import Header from "./components/Header";
 
 function App() {
   return (
-    <>
-      <div className="px-4 py-5 my-5 text-center">
-        <Container>
-          <Header />
-          <div className="col-lg-6 mx-auto">
-            <DisplayCounter />
-            <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-              <button
-                type="button"
-                className="btn btn-primary btn-lg px-4 gap-3"
-              >
-                Primary button
-              </button>
-              <button
-                type="button"
-                className="btn btn-outline-secondary btn-lg px-4"
-              >
-                Secondary
-              </button>
-            </div>
-          </div>
-        </Container>
-      </div>
-    </>
+    <center className="px-4 py-5 my-5 text-center">
+      <Container>
+        <Header />
+        <div className="col-lg-6 mx-auto">
+          <DisplayCounter />
+          <Controls />
+        </div>
+      </Container>
+    </center>
   );
 }
 
