9a9aef8662d394aabd7ff8d4c1acd8c2a7ccfcda
tanvirtbf <tanver018765@gmail.com>
2024-04-22 10:23:43 +0600

    connect redux with react using Provider

--- Redux with React (Kg Coding)/src/main.jsx
+++ Redux with React (Kg Coding)/src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import App from "./App.jsx";
+import counterStore from "./store/index.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    Provider
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={counterStore}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
