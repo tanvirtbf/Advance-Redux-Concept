9ee709d989b63358d24c951781a579d2a824e5d1
tanvirtbf <tanver018765@gmail.com>
2024-04-22 10:28:55 +0600

    use useSelector hook in DisplayCounter component for showing counter current value

--- Redux with React (Kg Coding)/src/components/DisplayCounter.jsx
+++ Redux with React (Kg Coding)/src/components/DisplayCounter.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const DisplayCounter = () => {
+
+  const counter = useSelector(store => store.counter)
+
   return (
     <p className="lead mb-4">
-      Counter Current Value
+      Counter Current Value : {counter}
     </p>
   );
 };
--- Redux with React (Kg Coding)/src/store/index.js
+++ Redux with React (Kg Coding)/src/store/index.js
@@ -1,6 +1,10 @@
 import {createStore} from 'redux'
 
-const counterReducer = (store, action) => {
+const INITIAL_VALUE = {
+  counter: 0
+}
+
+const counterReducer = (store = INITIAL_VALUE, action) => {
   return store;
 }
 
