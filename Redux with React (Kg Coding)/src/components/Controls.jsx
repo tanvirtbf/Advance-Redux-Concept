d5b87fd6c06a1ba26eddf70052842db4d8fee199
tanvirtbf <tanver018765@gmail.com>
2024-04-22 10:58:54 +0600

    create new section in Controls.jsx file

--- Redux with React (Kg Coding)/src/components/Controls.jsx
+++ Redux with React (Kg Coding)/src/components/Controls.jsx
@@ -3,18 +3,39 @@ import { useDispatch } from "react-redux";
 const Controls = () => {
   const dispatch = useDispatch();
   return (
-    <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-      <button
-        type="button"
-        className="btn btn-primary"
-        onClick={() => dispatch({ type: "increment", payload: 1 })}
-      >
-        +1
-      </button>
-      <button type="button" className="btn btn-success" onClick={() => dispatch({type: 'decrement', payload: 1})}>
-        -1
-      </button>
-    </div>
+    <>
+      <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={() => dispatch({ type: "increment", payload: 1 })}
+        >
+          +1
+        </button>
+        <button
+          type="button"
+          className="btn btn-success"
+          onClick={() => dispatch({ type: "decrement", payload: 1 })}
+        >
+          -1
+        </button>
+      </div>{" "}
+      <div className="d-grid gap-2 d-sm-flex justify-content-sm-center control-row">
+        <input type="text" placeholder="Enter Number" className="number-input"/>
+        <button
+          type="button"
+          className="btn btn-info"
+        >
+          ADD
+        </button>
+        <button
+          type="button"
+          className="btn btn-danger"
+        >
+          Subtract
+        </button>
+      </div>
+    </>
   );
 };
 
