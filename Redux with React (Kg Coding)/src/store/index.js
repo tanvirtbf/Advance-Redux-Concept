fffc94a1300725294edc0656a39d11fa0feb4eaf
tanvirtbf <tanver018765@gmail.com>
2024-04-22 10:17:46 +0600

    create store, and reducer function

---
+++ Redux with React (Kg Coding)/src/store/index.js
@@ -0,0 +1,9 @@
+import {createStore} from 'redux'
+
+const counterReducer = (store, action) => {
+  return store;
+}
+
+const counterStore = createStore(counterReducer)
+
+export default counterStore;
