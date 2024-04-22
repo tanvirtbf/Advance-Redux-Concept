3c7d54166a71630ef1a47a84a1954ad10cb846ad
tanvirtbf <tanver018765@gmail.com>
2024-04-22 10:12:37 +0600

    project ui complete , now work with logic

--- Redux with React (Kg Coding)/src/components/Controls.jsx
+++ Redux with React (Kg Coding)/src/components/Controls.jsx
@@ -3,11 +3,11 @@ import React from "react";
 const Controls = () => {
   return (
     <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-      <button type="button" className="btn btn-primary btn-lg px-4 gap-3">
-        Primary button
+      <button type="button" className="btn btn-primary">
+        +1
       </button>
-      <button type="button" className="btn btn-outline-secondary btn-lg px-4">
-        Secondary
+      <button type="button" className="btn btn-success">
+        -1
       </button>
     </div>
   );
